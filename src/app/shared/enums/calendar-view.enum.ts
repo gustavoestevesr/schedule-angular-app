804cabc9c974bae94eb5f3dff3c6d1619c4d1717
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-05-10 22:35:32 -0300

    refactor: adjustment interface IAppointment with enum Colors

---
+++ src/app/shared/enums/calendar-view.enum.ts
@@ -0,0 +1,21 @@
+export enum CalendarView {
+  Month = 'month',
+  Week = 'week',
+  Day = 'day',
+}
+
+export enum Colors {
+  Default = 'rgb(0, 128, 0, 0.75)',
+  Blue = 'rgb(0, 0, 255, 0.75)',
+  Red = 'rgb(255, 0, 0, 0.75)',
+  Yellow = 'rgb(255, 255, 0, 0.75)',
+  Orange = 'rgb(255, 165, 0, 0.75)',
+  Purple = 'rgb(128, 0, 128, 0.75)',
+  Green = 'rgb(0, 128, 0, 0.75)',
+  Pink = 'rgb(255, 192, 203, 0.75)',
+  Brown = 'rgb(165, 42, 42, 0.75)',
+  Cyan = 'rgb(0, 255, 255, 0.75)',
+  Lime = 'rgb(0, 255, 0, 0.75)',
+  Gray = 'rgb(128, 128, 128, 0.75)'
+}
+
--- src/app/shared/models/appointment.model.ts
+++ src/app/shared/models/appointment.model.ts
@@ -1,8 +1,10 @@
+import { Colors } from "../enums/calendar-view.enum";
+
 export interface IAppointment {
   uuid?: string;
   date: Date;
   title: string;
   startTime: string;
   endTime: string;
-  color?: string;
+  color: Colors;
 }
