f0aefff9e4bc4e9f9d552301017488640f12b240
Gustavo Esteves <gustavoesteves@directon.com.br>
2025-05-11 20:53:14 -0300

    fix: fix import of animations async

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { routes } from './app.routes';
 import { MatNativeDateModule } from '@angular/material/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), importProvidersFrom(MatNativeDateModule)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), importProvidersFrom(MatNativeDateModule)]
 };
